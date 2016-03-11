aaca28aea71b7802a9762eed9429ff10ba9687b6
janghe11 <jang0913@gmail.com>
2016-03-12 01:14:37 +0900

    2016년 3월 11일 Chapter2

--- SimpleButton/app/src/main/java/com/jang/simplebutton/MainActivity.java
+++ SimpleButton/app/src/main/java/com/jang/simplebutton/MainActivity.java
@@ -1,14 +1,17 @@
-package com.jang.helloworld;
+package com.jang.simplebutton;
 
 import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -16,5 +19,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "눌렀습니다.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
